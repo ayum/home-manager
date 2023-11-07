d1633bdffb15292157d4fdb7f2e8c79a2eadeabc
~ayum <ayum@ayum.ru>
2023-11-07 15:34:59 +0300

    flake: refactor for future nixos configuration adaptation

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixos.url = "github:nixos/nixpkgs/nixos-unstable";
+    np.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixos, np, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = np.legacyPackages.${system};
     in {
       homeConfigurations."root" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
@@ -21,11 +23,16 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [
-          ./root.nix
+          ./home-manager/root.nix
         ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+      };
+      nixosConfigurations."dev.ayum.ru" = nixos.lib.nixosSystem {
+        modules = [
+          ./nixos/dev.ayum.ru.nix
+        ];
       };
     };
 }
