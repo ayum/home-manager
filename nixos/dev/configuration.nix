66ad16175c5856f5e2a95b5a0802b94151b05b52
~ayum <ayum@users.noreply.github.com>
2024-03-01 09:44:41 +0000

    nixos(dev): default locale

--- nixos/dev/configuration.nix
+++ nixos/dev/configuration.nix
@@ -95,6 +95,8 @@
     };
   };
 
+  i18n.defaultLocale = "ru_RU.UTF-8";
+
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
