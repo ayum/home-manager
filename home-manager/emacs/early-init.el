c2cb9c1985da308890b05a4c0ded468999b8addf
~ayum <ayum@ayum.ru>
2024-11-17 02:12:06 +0300

    hm(emacs): dark background on start

--- home-manager/emacs/early-init.el
+++ home-manager/emacs/early-init.el
@@ -1,2 +1,4 @@
 (when (boundp 'native-comp-eln-load-path)
   (startup-redirect-eln-cache (expand-file-name "~/.cache/eln-cache/")))
+
+(setq default-frame-alist '((background-color . "#000000") (ns-appearance . dark) (ns-transparent-titlebar . t) (ns-appearance . 'nil)))
